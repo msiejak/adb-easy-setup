a88da89f4042b0c87fe282f8929a5204b39a316f
Michael Siejak <msiejak02@gmail.com>
2021-03-25 19:56:37 -0400

    Update setup_win10.bat

--- setup_win10.bat
+++ setup_win10.bat
@@ -1,5 +1,5 @@
 ::============================
-:: This is liscenced under the GNU GENERAL PUBLIC LICENSE Version 3 -> https://www.gnu.org/licenses/gpl-3.0.en.html
+:: This file is licensed under the GNU GENERAL PUBLIC LICENSE Version 3 -> https://www.gnu.org/licenses/gpl-3.0.en.html
 :: View on Github -> https://github.com/msiejak/adb-easy-setup
 :: Created by Michael Siejak, MSIEJAK Development -> https://msiejak.dev
 ::
