bc40d75d1aaea34a5c4434df7dedfb19fb03369e
Michael Siejak <msiejak02@gmail.com>
2021-03-25 09:05:13 -0400

    Create setup script for windows

---
+++ setup_win10.bat
@@ -0,0 +1,7 @@
+@echo downloading into %cd%...
+@curl https://dl.google.com/android/repository/platform-tools_r31.0.1-windows.zip
+@echo extracting files... into %cd%\platform-tools
+@powershell -command "Expand-Archive %cd%\platform-tools_r31.0.1-windows.zip %cd%"
+@echo updating path...
+@setx path "%PATH%;%cd%\platform-tools" /M
+@echo finished
