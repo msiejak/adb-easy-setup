fcada701eb850d53520678be4e17f5eeca654379
Michael Siejak <msiejak02@gmail.com>
2021-03-25 19:55:20 -0400

    delete original file

--- setup_win10.bat
+++ setup_win10.bat
@@ -8,11 +8,13 @@
 :: 2. Run the file as an administrator
 ::============================
 @echo downloading into %cd%
-@curl https://dl.google.com/android/repository/platform-tools_r31.0.1-windows.zip --output platform-tools_r31.0.1-windows.zip
+@curl https://dl.google.com/android/repository/platform-tools_r31.0.1-windows.zip --output platform-tools.zip
 @echo extracting files into %cd%\platform-tools
-@powershell -command "Expand-Archive %cd%\platform-tools_r31.0.1-windows.zip %cd%" -Force
+@powershell -command "Expand-Archive %cd%\platform-tools.zip %cd%" -Force
+@echo deleting origional file
+@del /a platform-tools.zip
 @echo setting enviroment variables
-@echo updating path...
+@echo updating path
 @setx path "%PATH%;%cd%\platform-tools" /M
 @echo finished
 @echo you need to reboot the system to apply enviroment variables
