43a00463fef77c65681a169e895940e5a8b3641b
Michael Siejak <msiejak02@gmail.com>
2021-03-25 14:08:39 -0400

    fix bugs

--- setup_win10.bat
+++ setup_win10.bat
@@ -1,7 +1,8 @@
-@echo downloading into %cd%...
-@curl https://dl.google.com/android/repository/platform-tools_r31.0.1-windows.zip platform-tools.zip
-@echo extracting files... into %cd%\platform-tools
-@powershell -command "Expand-Archive %cd%\platform-tools_r31.0.1-windows.zip %cd%"
+@echo downloading into %cd%
+@curl https://dl.google.com/android/repository/platform-tools_r31.0.1-windows.zip --output platform-tools_r31.0.1-windows.zip
+@echo extracting files into %cd%\platform-tools
+@powershell -command "Expand-Archive %cd%\platform-tools_r31.0.1-windows.zip %cd%" -Force
+@echo setting enviroment variables
 @echo updating path...
 @setx path "%PATH%;%cd%\platform-tools" /M
 @echo finished
