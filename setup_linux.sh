02d851f7a3ec08489412c6b40c58a42ce0d104f5
msiejak <msiejak02@gmail.com>
2021-04-14 18:50:57 -0400

    Update script for linux

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # ============================
 # This file is licensed under the GNU GENERAL PUBLIC LICENSE Version 3 -> https://www.gnu.org/licenses/gpl-3.0.en.html
 # View on Github -> https://github.com/msiejak/adb-easy-setup
@@ -6,7 +7,6 @@
 # Instructions:
 # 1. Run the file
 # ============================
-#!/bin/bash
 
 sudo apt install -y adb
 sudo apt-get install -y android-sdk-platform-tools-common
