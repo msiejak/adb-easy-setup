39f5c19c6a0a6698e23e32f28bf47319f966370c
Michael Siejak <msiejak02@gmail.com>
2021-08-04 19:54:44 -0400

    Update setup_linux.sh

--- setup_linux.sh
+++ setup_linux.sh
@@ -9,6 +9,7 @@
 # ============================
 
 sudo apt install -y adb
+sudo apt install -y fastboot
 sudo apt-get install -y android-sdk-platform-tools-common
 sudo usermod -aG plugdev $LOGNAME
-echo "setup complete - you need to reboot to apply changes"
+printf "\nsetup complete - you need to reboot to apply changes"
